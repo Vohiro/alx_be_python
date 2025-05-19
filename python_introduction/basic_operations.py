3c88f1232e7def1f3d3f1e2b239c4124667063d6
Vohiro <dvohiro@gmail.com>
2025-05-19 16:09:48 +0100

    Basic arithemtic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10;
+number2 = 5;
+
+Addition = number1 + number2;
+subtraction = number1 - number2;
+Product = number1 * number2;
+
+print('Addition of '+ str(number1) + ' and ' + str(number2) + ' is ' + str(Addition));
+print('Subtraction of '+ str(number1) + ' and ' + str(number2) + ' is ' + str(subtraction));
+print('Multiplication of '+ str(number1) + ' and ' + str(number2) + ' is ' + str(Product));
