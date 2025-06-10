a76c7577eb783a83d87f945e45aa6576ba19497e
Vohiro <dvohiro@gmail.com>
2025-06-10 17:49:06 +0100

    Creating a simple account class - OOP

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        # Initialize account_balance (defaults to 0 if not provided)
+        self.account_balance = account_balance
+        
+    def deposit(self, amount):
+        self.account_balance += amount
+        
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            success = True
+        else:
+            success = False
+        return success
+        
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
