20cf26d78ab92fbb64e4baa83d21a18baba57844
Vohiro <dvohiro@gmail.com>
2025-06-21 22:21:33 +0100

    Simple Library system using inheritance and composition

--- oop/library_system.py
+++ oop/library_system.py
@@ -7,7 +7,7 @@ class Book:
         print(f"{self.title} by {self.author}")
     
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
         
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
@@ -18,7 +18,7 @@ class EBook(Book):
         print(f"{self.title} by {self.author}, File Size: {self.file_size}KB")
         
     def __str__(self):
-        return f"{self.title} by {self.author}, File Size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
         
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
@@ -29,7 +29,7 @@ class PrintBook(Book):
         print(f"{self.title} by {self.author}, Page Count: {self.page_count}")
         
     def __str__(self):
-        return f"{self.title} by {self.author}, Page Count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
         
 class Library:
     def __init__(self):
